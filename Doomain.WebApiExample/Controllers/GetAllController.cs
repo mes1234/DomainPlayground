b1d7c5ab1627d6a9231d68f13eb912b999efa616
WK WK <witold.kepczynski@gmail.com>
2021-11-09 21:34:11 +0100

    swagger support

--- Doomain.WebApiExample/Controllers/GetAllController.cs
+++ Doomain.WebApiExample/Controllers/GetAllController.cs
@@ -1,25 +1,62 @@
-using Doomain.Abstraction;
 using Doomain.Example;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
-namespace Doomain.WebApiExample.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class GetAllController : ControllerBase
+namespace Doomain.WebApiExample.Controllers 
+{ 
+    /// <summary>
+    /// 
+    /// </summary>
+[ApiController]
+    public class DefaultApiController : ControllerBase
     {
-        private readonly IRepository<ModelA> _repository;
-
-        public GetAllController(IRepository<ModelA> repository)
+        /// <summary>
+        /// Returns a model A.
+        /// </summary>
+        /// <remarks>Returns a model A.</remarks>
+        /// <param name="userId">id if model</param>
+        /// <response code="200">A JSON array of models</response>
+        /// <response code="404">Not found</response>
+        /// <response code="500">Internal server error</response>
+        [HttpGet]
+        [Route("//modela/{userId}")]
+        public virtual IActionResult ModelaUserIdGet([FromRoute][Required] Guid userId)
         {
-            _repository = repository;
+        //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+        // return StatusCode(200, default(ModelA));
+
+        //TODO: Uncomment the next line to return response 404 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+        // return StatusCode(404);
+
+        //TODO: Uncomment the next line to return response 500 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+        // return StatusCode(500);
+         var   exampleJson = "{\n  \"name\" : \"dummy\",\n  \"id\" : \"75414b96-9aed-4a78-a956-2085d4e6d14b\",\n  \"revision\" : 0\n}";
+
+            var example = exampleJson != null
+            ? JsonConvert.DeserializeObject<Modeladto>(exampleJson)
+            : default(Modeladto);            //TODO: Change the data returned
+            return new ObjectResult(example);
         }
 
-        [HttpGet(Name = "GetInfo")]
-        public string Get()
+        /// <summary>
+        /// Add or update model A
+        /// </summary>
+        /// <param name="body">User credentials</param>
+        /// <param name="userId">id if model</param>
+        /// <response code="200">OK</response>
+        /// <response code="500">Internal server error</response>
+        [HttpPost]
+        [Route("//modela/{userId}")]
+        public virtual IActionResult ModelaUserIdPost([FromBody] Modeladto body, [FromRoute][Required] Guid userId)
         {
-           var m1 = _repository.Get(Guid.Parse("0721c89a-1437-4906-af53-da4d3880da6f"));
-            return m1.Name;
+            //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(200);
+
+            //TODO: Uncomment the next line to return response 500 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(500);
+
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Doomain.WebApiExample/Model/ModelA.Dto.cs
@@ -0,0 +1,9 @@
+﻿namespace Doomain.Example
+{
+    public class Modeladto
+    {
+        public string Name { get; set; } = "";
+        public string Id { get; set; } = "";
+        public int Revision { get; set; } = 1;
+    }
+}
