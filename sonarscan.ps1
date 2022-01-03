b61a0daa0345e6ca8e34d5b6dac1b668106c38b0
WK WK <witold.kepczynski@gmail.com>
2022-01-04 00:22:01 +0100

    done

--- sonarscan.ps1
+++ sonarscan.ps1
@@ -1,8 +1,8 @@
-dotnet sonarscanner begin /k:"domain-playground" /d:sonar.host.url="http://localhost:9000"  /d:sonar.login="5e498f7e509591486370fabc8a62a1e9e9e73e00"  /d:sonar.coverageReportPaths=".\sonarqubecoverage\SonarQube.xml"
+dotnet sonarscanner begin /k:"domain-playground" /d:sonar.host.url="http://localhost:9000" /d:sonar.login="5e498f7e509591486370fabc8a62a1e9e9e73e00" /d:sonar.coverageReportPaths=".\sonarqubecoverage\SonarQube.xml" /d:sonar.cs.xunit.reportsPaths="./*.Tests/TestResults/TestResults.xml"
 
 dotnet build
 
-dotnet test --no-build --collect:"XPlat Code Coverage"
+dotnet test --no-build --collect:"XPlat Code Coverage"  --logger:xunit
 
 reportgenerator "-reports:*\TestResults\*\coverage.cobertura.xml" "-targetdir:sonarqubecoverage" "-reporttypes:SonarQube"
  
