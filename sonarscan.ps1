7a47e82deaf57c7332ecb85bf90be5d2b2065faa
WK WK <witold.kepczynski@gmail.com>
2022-01-03 23:56:12 +0100

    coverage

---
+++ sonarscan.ps1
@@ -0,0 +1,10 @@
+dotnet sonarscanner begin /k:"domain-playground" /d:sonar.host.url="http://localhost:9000"  /d:sonar.login="5e498f7e509591486370fabc8a62a1e9e9e73e00"  /d:sonar.coverageReportPaths=".\sonarqubecoverage\SonarQube.xml"
+
+dotnet build
+
+dotnet test --no-build --collect:"XPlat Code Coverage"
+
+reportgenerator "-reports:*\TestResults\*\coverage.cobertura.xml" "-targetdir:sonarqubecoverage" "-reporttypes:SonarQube"
+ 
+
+dotnet sonarscanner end /d:sonar.login="5e498f7e509591486370fabc8a62a1e9e9e73e00"
